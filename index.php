f86c2a50a2e0193bdee05ed4499d008b5624c4e8
390622-sys <390622@guhsd.net>
2026-03-09 18:06:06 +0000

    checkpoint 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php // L1-A-HTMLShell-2026-03-09 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Forest Survival</title>
+        <link rel="stylesheet" href="data/style.css">
+    </head>
+<body>
+    <h1>Bunker Survival</h1>
+    <p>Day 1: The forest is quiet. You have 30 days to survive until the chopper arrives.</p>
+</body>
+</html>
